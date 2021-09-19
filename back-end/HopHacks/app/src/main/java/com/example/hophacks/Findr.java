b49e07aaa3e9d263ecc1e8b0a8a41588db57f743
scesar1 <scesar1@jhu.edu>
2021-09-19 01:54:43 -0400

    Changed button behavior for second fragment, created findr object

--- back-end/HopHacks/app/src/main/java/com/example/hophacks/Findr.java
+++ back-end/HopHacks/app/src/main/java/com/example/hophacks/Findr.java
@@ -1,4 +1,5 @@
 package com.example.hophacks;
+import android.app.Activity;
 import android.os.Build;
 import androidx.annotation.RequiresApi;
 import java.time.LocalTime;
@@ -6,17 +7,26 @@ import java.time.LocalTime;
 
 
 public class Findr {
+    public int score;
+    boolean foodAllowed;
+    boolean buyFood; // want to buy food? if yes = true
+    int desiredHygiene; // 0 = no bathroom, 1 = bathroom nearby,
+    // 2 = bathroom + hygiene products available
+    int desiredSeating; // 0 = no seating, 1 = desk chairs/basic seating,
+    // 2 = comfy (lounge) chairs, 3 = couches, extremely comfortable
+    int desiredPrivacy; // 0 = no privacy, 1 = desk cubicles, 2 = like 1,
+    // but nobody is gonna bother you, 3 = locked doors
+    boolean vendingFoodDesired; // true if you want vending machine snacks
+    // boolean wantToRsvp; // do you want to have to reserve it?
+    int desiredVolume; // 0 = NO TALKING, 1 = very quiet, whisper only, 2 = convos allowed,
+    // 3 = CONVOS WILL BE HAD HERE
+    boolean claustrophobicDesired; // do you want to be in an enclosed space?
+    boolean vendingSuppliesDesired; // do you want stationery vending? pencils, pens, etc
+    boolean[] equipmentDesired; // {computer, projector, whiteboard, printer}
     @RequiresApi(api = Build.VERSION_CODES.O)
-    public static void main(String[] args){
+    public Findr(){
 
         // boolean jCardHad; // do you have a jCard? if yes = true
-
-        boolean foodAllowed;
-        boolean buyFood; // want to buy food? if yes = true
-        int desiredHygiene; // 0 = no bathroom, 1 = bathroom nearby,
-                            // 2 = bathroom + hygiene products available
-        int desiredSeating; // 0 = no seating, 1 = desk chairs/basic seating,
-                            // 2 = comfy (lounge) chairs, 3 = couches, extremely comfortable
         int currentTime = (LocalTime.now().getHour() * 100) + LocalTime.now().getMinute(); //local time
         int timeOn2600Scale = currentTime;
         //System.out.println(currentTime);
@@ -25,17 +35,8 @@ public class Findr {
             timeOn2600Scale += 2400;
         }
 
-        int desiredPrivacy; // 0 = no privacy, 1 = desk cubicles, 2 = like 1,
-                            // but nobody is gonna bother you, 3 = locked doors
-        boolean vendingFoodDesired; // true if you want vending machine snacks
-        // boolean wantToRsvp; // do you want to have to reserve it?
-        int desiredVolume; // 0 = NO TALKING, 1 = very quiet, whisper only, 2 = convos allowed,
-                            // 3 = CONVOS WILL BE HAD HERE
-        boolean claustrophobicDesired; // do you want to be in an enclosed space?
-        boolean vendingSuppliesDesired; // do you want stationery vending? pencils, pens, etc
-        boolean[] equipmentDesired; // {computer, projector, whiteboard, printer}
-
-
-
+    }
+    public void updateScore(int score) {
+        this.score += score;
     }
 }
--- back-end/HopHacks/app/src/main/java/com/example/hophacks/SecondFragment.java
+++ back-end/HopHacks/app/src/main/java/com/example/hophacks/SecondFragment.java
@@ -1,20 +1,23 @@
 package com.example.hophacks;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
 import com.example.hophacks.databinding.FragmentSecondBinding;
 
+@RequiresApi(api = Build.VERSION_CODES.O)
 public class SecondFragment extends Fragment {
 
     private FragmentSecondBinding binding;
-
+    Findr finderScore = new Findr();
     @Override
     public View onCreateView(
             LayoutInflater inflater, ViewGroup container,
@@ -32,8 +35,8 @@ public class SecondFragment extends Fragment {
         binding.imageButton2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                NavHostFragment.findNavController(SecondFragment.this)
-                        .navigate(R.id.action_SecondFragment_to_FirstFragment);
+                final int desiredVolume = 0;
+                finderScore.updateScore(desiredVolume);
             }
         });
     }
